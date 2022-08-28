cc56137652ddf59ed2a9dfd98e29d6acf071fef8
zhaosi <zhaosi@git.com>
2022-08-28 21:17:57 +0800

    1

--- config/filesystems.php
+++ config/filesystems.php
@@ -173,7 +173,7 @@ return [
         ],
         'sftp1' => [
             'driver' => 'ftp',
-            'host' => env('SFTP_HOST1',''),
+            'host' => env('SFTP_HOST',''),
             'username' => env('SFTP_USERNAME',''),
             'password' => env('SFTP_PASSWORD',''),
 
