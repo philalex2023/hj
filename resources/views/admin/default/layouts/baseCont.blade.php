2e0259fbc20767ce98577d02fb77c197a86d0141
zhaosi <zhaosi@git.com>
2022-09-08 12:43:31 +0800

    1

--- resources/views/admin/default/layouts/baseCont.blade.php
+++ resources/views/admin/default/layouts/baseCont.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    @if(env('APP_ENV')=='production')
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    @endif
     @section('title')
         <title>{{ $title ?? config('copyright.system_name') }}</title>
         <meta name="keywords" content="{{ $keyword ?? config('copyright.system_name') }}">
