2eb664982a477243dd18dc2b5004307ce1fd8a6d
zhaosi <zhaosi@git.com>
2022-09-08 17:29:26 +0800

    1

--- resources/views/admin/default/adminPermission/index.blade.php
+++ resources/views/admin/default/adminPermission/index.blade.php
@@ -7,13 +7,15 @@
 @section('foot_js')
     @include($base_blade_path.'.tpl.listConfig')
     <script>
-        //表格排序修改补刷新表格数据
+        //表格排序修改不刷新表格数据
         g_sort_reload=false;
         //树形table，不支持分页，不支持异步,异步这块后续再增加，暂时不加
         layui.use(['treeListTable',], function () {
             var treeListTable = layui.treeListTable;
             var cols = @json($cols);
-            treeListTable.render(listConfig.index_url, cols,{treeDefaultClose:1});
+            treeListTable.render(listConfig.index_url, cols,{treeDefaultClose:1,where: {
+
+                }});
 
         });
     </script>
