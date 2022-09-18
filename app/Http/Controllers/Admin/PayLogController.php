7ffc8eb1ce93275c1eeb1d1f701aae0bebe38d7e
zhaosi <zhaosi@git.com>
2022-09-18 15:54:15 +0800

    1

--- app/Http/Controllers/Admin/PayLogController.php
+++ app/Http/Controllers/Admin/PayLogController.php
@@ -120,6 +120,9 @@ class PayLogController extends BaseCurlController
 
     public function setListOutputItemExtend($item)
     {
+        if(!$item){
+            return parent::setListOutputItemExtend($item);
+        }
         $deviceSystems = [
             0 => '',
             1 => 'ios轻量版',
