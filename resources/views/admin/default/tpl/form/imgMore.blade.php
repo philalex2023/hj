b16d9fd4faa7a8db4a1ff660641931e998769538
zhaosi <zhaosi@git.com>
2022-09-21 21:55:29 +0800

    1

--- resources/views/admin/default/tpl/form/imgMore.blade.php
+++ resources/views/admin/default/tpl/form/imgMore.blade.php
@@ -22,14 +22,14 @@
                  data-oss_type="{{ ($v['oss_type'] ?? '') }}"
                  data-path="{{ $v['path'] }}" data-view_src="{{ ($v['view_src'] ?? '') }}"
                   data-origin_path="{{ $v['origin_path'] }}">
-                <div class="file-choose-list-item-img " ui-event="viewImg" data-src="{{ $v['view_src']??'' }}" style="background-image: url('{{ $v['view_src']??'' }}') ">
+                <div class="file-choose-list-item-img preview-img" ui-event="viewImg" data-src="{{ $v['view_src']??'' }}" style="background-image: url('{{ $v['view_src']??'' }}') ">
 
                 </div>
                 <div class=" handle ">
-                    <button type="button" class="layui-btn layui-btn-primary layui-btn-sm js_left_pic" data-tips="tooltip" title="{{ lang('左移') }}" >
+                    {{--<button type="button" class="layui-btn layui-btn-primary layui-btn-sm js_left_pic" data-tips="tooltip" title="{{ lang('左移') }}" >
                         <i class="layui-icon layui-icon-left"></i></button>
                     <button type="button" class="layui-btn layui-btn-primary layui-btn-sm js_right_pic" data-tips="tooltip" title="{{ lang('右移') }}">
-                        <i class="layui-icon layui-icon-right"></i></button>
+                        <i class="layui-icon layui-icon-right"></i></button>--}}
                     <button type="button" class="layui-btn layui-btn-primary layui-btn-sm js_remove_pic" data-tips="tooltip" title="{{ lang('移除') }}" >
                         <i class="layui-icon layui-icon-delete"></i></button>
                 </div>
@@ -46,8 +46,22 @@
             data-target="#{{ (md5($form_item['field'])) }}"
             data-event="upload" data-more="1"
             class="layui-btn layui-btn-sm  mr-10"><i class="layui-icon layui-icon-add-1"></i> {{ lang('点击上传') }}</button>
-    <button type="button" data-more="1"   data-event="uploadPlace"
+    {{--<button type="button" data-more="1"   data-event="uploadPlace"
             {{ $form_item['place_attr']??'' }}
-            data-target="#{{ (md5($form_item['field']))  }}" class="layui-btn   layui-btn-sm"><i class="layui-icon layui-icon-picture"></i> {{ lang('库选择') }}</button>
+            data-target="#{{ (md5($form_item['field']))  }}" class="layui-btn   layui-btn-sm"><i class="layui-icon layui-icon-picture"></i> {{ lang('库选择') }}</button>--}}
 
 </div>
+<script>
+    let xhr = new XMLHttpRequest();
+    $('.preview-img').forEach(item)
+    {
+        let src = "{{ \App\TraitClass\VideoTrait::getDomain(2) }}"+$(item).attr('data-src');
+        xhr.open('get',src);
+        xhr.responseType = 'blob';
+        xhr.onload = function () {
+            $(item).attr('data-src',window.URL.createObjectURL(xhr.response));
+        }
+        xhr.send();
+    }
+
+</script>
