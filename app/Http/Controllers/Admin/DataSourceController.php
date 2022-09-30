14a30c068e02ffbc170cb577ce5f31f612d50399
zhaosi <zhaosi@git.com>
2022-09-30 23:09:22 +0800

    1

--- app/Http/Controllers/Admin/DataSourceController.php
+++ app/Http/Controllers/Admin/DataSourceController.php
@@ -200,7 +200,7 @@ class DataSourceController extends BaseCurlController
                         foreach ($items as $item){
                             $jsonArr = json_decode($item->tag,true);
                             $intersect = array_intersect($jsonArr,$tagIds); //交集
-                            if(in_array(181,$jsonArr)){
+                            if(in_array("181",$jsonArr)){
                                 Log::info('test',[$jsonArr,$tagIds,$intersect]);
                             }
                             if(!empty($intersect)){
