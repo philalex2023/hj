7ade04057dfa06ebff5bf4a57463217722c856cc
zhaosi <zhaosi@git.com>
2022-10-06 11:45:49 +0800

    1

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -93,7 +93,7 @@ class HomeController extends Controller
         if(isset($request->params)){
             $user = $request->user();
             $params = self::parse($request->params);
-            Log::info('list_params',[$params]);
+            //Log::info('list_params',[$params]);
             $validated = Validator::make($params,[
                 'cid' => 'required|integer',
                 'page' => 'required|integer',
@@ -110,10 +110,10 @@ class HomeController extends Controller
             $res = json_decode($res,true);
             if(!$res || $redis->get('homeLists_fresh')){
                 $lock = Cache::lock('homeLists_lock');
-                if(!$lock->get()){
+                /*if(!$lock->get()){
                     Log::info('index_list',[$sectionKey]);
                     return response()->json(['state' => -1, 'msg' => '服务器繁忙请稍候重试']);
-                }
+                }*/
 
                 $paginator = DB::table('topic')->where('cid',$cid)->where('status',1)->orderBy('sort')->simplePaginate($perPage,['id','name','show_type','contain_vids'],'homeContent',$page);
                 $res['hasMorePages'] = $paginator->hasMorePages();
