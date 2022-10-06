a634132d0d61436c4d614c81b09dd9070113fac0
zhaosi <zhaosi@git.com>
2022-10-06 14:07:59 +0800

    1

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -157,7 +157,7 @@ class HomeController extends Controller
                                                     ]
                                                 ]
                                             ],
-                                            'script_score' => [
+                                            /*'script_score' => [
                                                 'script' => [
                                                     'lang' => 'painless',
                                                     'params' => [
@@ -165,7 +165,7 @@ class HomeController extends Controller
                                                     ],
                                                     'source' => "for(i in params.scoring) { if(doc['id'].value == i.id ) return i.score; } return 0;"
                                                 ]
-                                            ]
+                                            ]*/
                                         ]
                                     ]
                                 ],
@@ -173,7 +173,7 @@ class HomeController extends Controller
                             $topic['style'] = (string)$topic['style']; //android要是字符串
                             $es = $this->esClient();
                             $response = $es->search($searchParams);
-                            Log::info('index_list_res',$response);
+                            //Log::info('index_list_res',$response);
                             if(isset($response['hits']) && isset($response['hits']['hits'])){
                                 foreach ($response['hits']['hits'] as $item) {
                                     $videoList[] = $item['_source'];
