c72401e0f48a7059b5e0a41cce485b79f2b6bd15
zhaosi <zhaosi@git.com>
2022-10-06 17:03:15 +0800

    1

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -14,12 +14,12 @@ class ConfigController extends Controller
 
     public function ack(): \Illuminate\Http\JsonResponse
     {
-        Log::info('==ack==',[request()->all()]);
         $configKey = 'api_config';
         $configData = $this->redis()->get($configKey);
         $res = $configData ? (array)json_decode($configData,true) : $this->getConfigDataFromDb();
         $this->frontFilterAd($res['open_screen_ads']);
         $this->frontFilterAd($res['activity_ads']);
+        Log::info('==ack==',[$res]);
         return response()->json([
             'state'=>0,
             'data'=>$res
