82509cad149ae8a462fb4e690f5b9959887dd581
zhaosi <zhaosi@git.com>
2022-10-18 14:35:54 +0800

    1

--- app/TraitClass/BbsTrait.php
+++ app/TraitClass/BbsTrait.php
@@ -71,10 +71,6 @@ trait BbsTrait
             $list[$k]['all_game_gold'] = $this->getAllGameNeedGold();
             $list[$k]['official_type'] = (int)($re['official_type']??0);
 
-            //$list[$k]['video_picture'] = [];
-            if($re['id']==2297){
-                Log::info('TEST_commBbs',[$re]);
-            }
             if (isset($re['video_picture'])) {
                 $videoPictures = is_array($re['video_picture']) ? $re['video_picture'] : json_decode($re['video_picture'],true) ;
                 isset($videoPictures[0]) && $list[$k]['video_picture'] = [$domainSync . $videoPictures[0]];
