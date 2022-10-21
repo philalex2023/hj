41cf100802d9946b399d08326a7b8e8f3bf0b562
zhaosi <zhaosi@git.com>
2022-10-21 15:59:14 +0800

    1

--- app/Console/Commands/RepairVideo.php
+++ app/Console/Commands/RepairVideo.php
@@ -55,8 +55,11 @@ class RepairVideo extends Command
         $bar->start();
 
         $bar->advance();
+
+        $ids = '';
         foreach ($Items as $item)
         {
+            $ids .= $item->id;
             $this->info('can delete '.$item->id.' status '.$item->status);
             /*$file_name = pathinfo($item->url,PATHINFO_FILENAME);
             $tmp_path = '/home/hj/public/slice/hls/'.$file_name.'/';
@@ -67,6 +70,7 @@ class RepairVideo extends Command
             $this->dispatch($job->onQueue('high'));*/
         }
         $bar->finish();
+        $this->info('ids: '.$ids);
         $this->info('######执行成功######');
         return 0;
     }
