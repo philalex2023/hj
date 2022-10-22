3e47d89464ea7b2669f5661f74b586b3aef7be96
zhaosi <zhaosi@git.com>
2022-10-22 16:36:25 +0800

    1

--- app/Http/Controllers/Api/RechargeController.php
+++ app/Http/Controllers/Api/RechargeController.php
@@ -24,24 +24,4 @@ class RechargeController extends \App\Http\Controllers\Controller
         ]);
     }
 
-    /*public function submit(Request $request)
-    {
-        if(isset($request->params)) {
-            $params = ApiParamsTrait::parse($request->params);
-            Validator::make($params, [
-                'amount' => 'required|integer',
-                'id' => 'required|integer',
-                'rechargeType' => 'required|integer',
-                'orderType' => 'required|integer',
-            ])->validate();
-            //todo 加入扣量统计表
-
-            return response()->json([
-                'state'=>0,
-                'msg'=>'提交成功',
-                'data'=>[]
-            ]);
-        }
-        return [];
-    }*/
 }
