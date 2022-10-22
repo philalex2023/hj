a1c2d70e73e684ac29eea1318da57351cf7ed934
zhaosi <zhaosi@git.com>
2022-10-22 17:34:20 +0800

    1

--- app/Models/PayLog.php
+++ app/Models/PayLog.php
@@ -6,7 +6,7 @@ class PayLog extends BaseModel
 {
     protected $table = 'pay_log';
 
-    public function order(){
+    /*public function order(){
         return $this->hasOne(Order::class,'id','order_id');
-    }
+    }*/
 }
