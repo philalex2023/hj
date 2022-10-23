ed4a033c7cdcdd71a1cbf3a8f8885867d22f346e
zhaosi <zhaosi@git.com>
2022-10-23 20:15:27 +0800

    1

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -32,47 +32,49 @@ class ConfigController extends Controller
 
     public function pullOriginVideo(Request $request): \Illuminate\Http\JsonResponse
     {
-        $ip = $this->getRealIp();
-        if($ip == '154.207.98.132'){
-            $size = 100;
-            $page = $request->get('page',1);
-            $offset = ($page-1)*$size;
-            $source = ['id','is_top','name','gold','cat','tag_kv','sync','title','dash_url','hls_url','duration','type','restricted','cover_img','updated_at'];
-            $searchParams = [
-                'index' => 'video_index',
-                'body' => [
-                    'track_total_hits' => true,
-                    'size' => $size,
-                    'from' => $offset,
-                    '_source' => $source,
-                    'query' => [
-                        'bool'=>[
-                            'must' => [
-                                ['term' => ['type'=>4]],
-                            ]
-                        ]
-                    ],
-                ],
-            ];
-
-            $es = $this->esClient();
-            $response = $es->search($searchParams);
-            //Log::info('searchParam_home_list',[json_encode($searchParams)]);
-            $videoList = [];
-            if(isset($response['hits']) && isset($response['hits']['hits'])){
-                foreach ($response['hits']['hits'] as $item) {
-                    $videoList[] = $item['_source'];
-                }
-            }
+        /*$ip = $this->getRealIp();
+        if(!$ip == '154.207.98.132') {
             return response()->json([
-                'state'=>0,
-                'data'=>$videoList
+                'state'=>401,
+                'data'=>[]
             ]);
+        }*/
+        $size = 100;
+        $page = $request->get('page',1);
+        $offset = ($page-1)*$size;
+        $source = ['id','is_top','name','gold','cat','tag_kv','sync','title','dash_url','hls_url','duration','type','restricted','cover_img','updated_at'];
+        $searchParams = [
+            'index' => 'video_index',
+            'body' => [
+                'track_total_hits' => true,
+                'size' => $size,
+                'from' => $offset,
+                '_source' => $source,
+                'query' => [
+                    'bool'=>[
+                        'must' => [
+                            ['term' => ['type'=>4]],
+                        ]
+                    ]
+                ],
+            ],
+        ];
+
+        $es = $this->esClient();
+        $response = $es->search($searchParams);
+        //Log::info('searchParam_home_list',[json_encode($searchParams)]);
+        $videoList = [];
+        if(isset($response['hits']) && isset($response['hits']['hits'])){
+            foreach ($response['hits']['hits'] as $item) {
+                $videoList[] = $item['_source'];
+            }
         }
         return response()->json([
-            'state'=>401,
-            'data'=>[]
+            'state'=>0,
+            'data'=>$videoList
         ]);
     }
+
+
 
 }
