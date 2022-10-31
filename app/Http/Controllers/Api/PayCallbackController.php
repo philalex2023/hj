df82fc6e460236774730e93c3853f8ffe956d2dd
zhaosi <zhaosi@git.com>
2022-10-31 22:03:20 +0800

    1

--- app/Http/Controllers/Api/PayCallbackController.php
+++ app/Http/Controllers/Api/PayCallbackController.php
@@ -4,18 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
-use App\TraitClass\ApiParamsTrait;
-use App\TraitClass\PayCallbackTrait;
 use App\TraitClass\PaySignVerifyTrait;
 use App\TraitClass\PayTrait;
 use App\TraitClass\IpTrait;
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Psr\SimpleCache\InvalidArgumentException;
+
 
 /**
  * 通达支付
