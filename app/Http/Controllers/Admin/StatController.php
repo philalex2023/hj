d0befc1b202216f4a7a508a89c9a9a95a3e0fffb
zhaosi <zhaosi@git.com>
2022-11-02 14:20:09 +0800

    1

---
+++ app/Http/Controllers/Admin/StatController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Admin;
+use App\Models\AdminLog;
+use App\Models\HjStatisticsDay;
+use App\Models\Role;
+
+class StatController extends BaseCurlIndexController
+{
+    //页面信息
+    public $pageName = '统计列表';
+
+    //1.设置模型
+    public function setModel()
+    {
+        $this->model = new HjStatisticsDay();
+
+    }
+
+
+    //2.首页设置列表显示的信息
+    public function indexCols(): array
+    {
+        return [
+            [
+                'field' => 'active_user',
+                'width' => 80,
+                'title' => '日活',
+                'sort' => 1,
+                'align' => 'center'
+            ],
+            [
+                'field' => 'online_user',
+                'minWidth' => 80,
+                'title' => '在线用户',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'keep_1',
+                'width' => 80,
+                'title' => '留存',
+                'align' => 'center',
+            ],
+            [
+                'field' => 'inc_user',
+                'width' => 80,
+                'title' => '管理员昵称',
+                'align' => 'center'
+
+            ],
+            [
+                'field' => 'inc_android_user',
+                'width' => 80,
+                'title' => '新增安卓用户',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'inc_ios_user',
+                'minWidth' => 80,
+                'title' => '新增苹果用户',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'gold_recharge',
+                'width' => 80,
+                'title' => '金币充值',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'vip_recharge',
+                'width' => 80,
+                'title' => 'VIP充值',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'new_user_recharge',
+                'width' => 150,
+                'title' => '新增用户充值',
+                'align' => 'center'
+            ],
+            [
+                'field' => 'old_user_recharge',
+                'width' => 150,
+                'title' => '老增用户充值',
+                'align' => 'center'
+            ],
+
+
+        ];
+    }
+
+    //3.设置搜索部分
+    public function setOutputSearchFormTpl($shareData)
+    {
+        $data = [
+            [
+                'field' => 'query_at_time',
+                'type' => 'date',
+//                'attr' => 'data-range=true',
+                'attr' => 'data-range=~',//需要特殊分割
+                'name' => '时间范围',
+            ],
+
+        ];
+        //赋值到ui数组里面必须是`search`的key值
+        $this->uiBlade['search'] = $data;
+    }
+
+    public function handleResultModel($model): array
+    {
+        $atTime = $this->rq->input('at_time');
+        if($atTime!=null){
+            if($atTime > 0){
+                $timeRangeArr = explode('~',$atTime);
+                $model = $model->where('at_time','>=',$timeRangeArr[0])->where('at_time','<=',$timeRangeArr[1]);
+            }
+        }
+        return parent::handleResultModel($model);
+    }
+
+}
