3e312bf3812eac2d5c32b8ec6aa4fbeb574c83da
zhaosi <zhaosi@git.com>
2022-11-11 12:57:45 +0800

    1

--- app/Http/Controllers/Admin/RechargeChannelsController.php
+++ app/Http/Controllers/Admin/RechargeChannelsController.php
@@ -33,7 +33,7 @@ class RechargeChannelsController extends BaseCurlController
 
     public function getPayChannels($code=false): array
     {
-        $channels = RechargeChannel::query()->where('status',1)->get(['id','name','zfb_code','wx_code']);
+        $channels = RechargeChannel::query()->where('status',1)->get(['id','name','zfb_code','wx_code'])->toArray();
         if(!$code){
             $selector = [''=>['id'=>'','name'=>'选择充值渠道']];
             foreach ($channels as $channel){
