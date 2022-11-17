6c9e3b1a1db086811e55c02ccf9f895b8cb9a29b
zhaosi <zhaosi@git.com>
2022-11-17 14:13:08 +0800

    1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
 
         'api' => [
 //            'throttle:30,1',
-             'throttle:600,1', //一分钟600次
+             'throttle:120,1', //一分钟600次
             //'auth:api', //路由添加
             //'secret',
             \Illuminate\Routing\Middleware\SubstituteBindings::class
