eb159979ce31ed71e1bcb67be3c9e3934274d792
zhaosi <zhaosi@git.com>
2022-11-23 20:55:32 +0800

    1

--- app/TraitClass/LoginTrait.php
+++ app/TraitClass/LoginTrait.php
@@ -72,14 +72,14 @@ trait LoginTrait
 
     public function getDidFromDb($did,$loginRedis)
     {
-        if(!$loginRedis->setnx('reg_lock_test',1)){
+        if(!$loginRedis->setnx('lock_login_did',1)){
             return response()->json(['state' => -1, 'msg' => '服务器繁忙请稍候重试']);
         }
         /*$ids = Live::query()->where('status',1)->pluck('id')->all();
         $this->redis()->sAddArray('fakeLiveIdsCollection',$ids);*/
         $didArr = User::query()->pluck('did')->all();
         $loginRedis->sAddArray('account_did',$didArr);
-        $loginRedis->del('reg_lock');
+        $loginRedis->del('lock_login_did');
         return User::query()->where('did',$did)->exists();
     }
 
