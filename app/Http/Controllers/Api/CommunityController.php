17f34ed472cd5dde0e96cc6816a6d060fc6184d5
zhaosi <zhaosi@git.com>
2022-12-02 16:26:17 +0800

    1

--- app/Http/Controllers/Api/CommunityController.php
+++ app/Http/Controllers/Api/CommunityController.php
@@ -49,18 +49,52 @@ class CommunityController extends Controller
         return response()->json($res);
     }
 
+    function  mdate($time = NULL): string
+    {
+        $time  =  $time === NULL ||  $time  > time() ? time() :  intval ( $time );
+        $t  = time() -  $time ;  //时间差 （秒）
+//        $y  =  date ( 'Y' ,  $time )- date ( 'Y' , time()); //是否跨年
+        return match ($t) {
+            $t == 0 => '刚刚',
+            $t < 60 => $t . '秒前',
+            $t < 60 * 60 => floor($t / 60) . '分钟前',
+            $t < 60 * 60 * 24 => floor($t / (60 * 60)) . '小时前',
+            $t < 60 * 60 * 24 * 7 => floor($t / (60 * 60 * 24)) . '天前',
+            $t < 60 * 60 * 24 * 30 => floor($t / (60 * 60 * 24 * 7)) . '周前',
+            $t < 60 * 60 * 24 * 365 => floor($t / (60 * 60 * 24 * 30)) . '月前',
+//            $t < 60 * 60 * 24 * 3 => floor($time / (60 * 60 * 24)) == 1 ? '昨天 ' . date('H:i', $time) : '前天 ' . date('H:i', $time),
+//            $t < 60 * 60 * 24 * 30 => date('m月d日 H:i', $time),
+//            $t < 60 * 60 * 24 * 365 && $y == 0 => date('m月d日', $time),
+            default => date('Y年m月d日', $time),
+        };
+    }
+
     public function focus(Request $request): \Illuminate\Http\JsonResponse
     {
         /*if(!$request->user()){
             return response()->json([]);
         }*/
         //todo cache
+        $ids = []; //todo
         //热门圈子
         $hotCircle = DB::table('circle')->orderByDesc('many_friends')->limit(8)->get(['id','name','background as imgUrl','many_friends as user']);
         //我加入的圈子
-        $joinCircle = DB::table('circle')->orderByDesc('introduction')->limit(8)->get(['id','name','background as imgUrl']);
+        $joinCircle = DB::table('circle')
+//            ->whereIn('id',$ids)
+            ->orderByDesc('id')
+            ->limit(8)->get(['id','name','background as imgUrl']);
         //来自我关注的圈子
-        $fromMeFocusCircle = DB::table('circle_topic')->orderByDesc('introduction')->limit(8)->get(['id','name','avatar','introduction as des','background as imgUrl','many_friends as user']);
+
+        $fromMeFocusCircle = DB::table('circle_topic')
+//            ->whereIn('id',$ids)
+            ->orderByDesc('introduction')
+            ->limit(8)->get([
+            'id','name','circle_name','avatar','author','scan','comments','likes','introduction as des','many_friends as user','album','created_at','tag_kv'
+        ]);
+        foreach ($fromMeFocusCircle as &$item){
+            $item['tag_kv'] = json_decode($item->tag_kv,true) ?? [];
+            $item['created_at'] = $this->mdate(strtotime($item['created_at']));
+        }
 
         $data = [
             [
